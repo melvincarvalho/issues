d2db005410cfbc5a26f51f3cd346bac365fcbcc7
Melvin Carvalho <melvincarvalho@gmail.com>
2019-10-01 21:57:40 +0200

    first issue

---
+++ 2019/10/01/lapps.mm
@@ -0,0 +1,122 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="include lapps node" FOLDED="false" ID="ID_1831615648" CREATED="1569272472516" MODIFIED="1569958945696" LINK="index.mm" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="todo" POSITION="right" ID="ID_640740799" CREATED="1569947415454" MODIFIED="1569947415836">
+<icon BUILTIN="idea"/>
+<edge COLOR="#ff0000"/>
+<node TEXT="github" ID="ID_1201621376" CREATED="1569947794812" MODIFIED="1569958968311">
+<node TEXT="repo" ID="ID_468197740" CREATED="1569947847293" MODIFIED="1569947854696">
+<node TEXT="store" ID="ID_1402507020" CREATED="1569948601869" MODIFIED="1569948605262">
+<node TEXT="default" ID="ID_1077496639" CREATED="1569948669735" MODIFIED="1569948672505">
+<node TEXT="mc issues" ID="ID_1835624199" CREATED="1569948673547" MODIFIED="1569948677152">
+<node ID="ID_865195012" CREATED="1569948736656" MODIFIED="1569948736656" LINK="https://github.com/melvincarvalho/issues"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://github.com/melvincarvalho/issues">https://github.com/melvincarvalho/issues</a> 
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="reference gh issue" ID="ID_722534938" CREATED="1569950427783" MODIFIED="1569950432356"/>
+</node>
+</node>
+</node>
+<node TEXT="issue" ID="ID_281621243" CREATED="1569958905439" MODIFIED="1569958906828">
+<node TEXT="https://github.com/solidpayorg/lightning.money/issues/1" ID="ID_1182299254" CREATED="1569958913712" MODIFIED="1569958913712" LINK="https://github.com/solidpayorg/lightning.money/issues/1"/>
+</node>
+</node>
+</node>
+<node TEXT="doing" POSITION="right" ID="ID_1487179483" CREATED="1569947415875" MODIFIED="1569947415899">
+<icon BUILTIN="bookmark"/>
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="done" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" POSITION="left" ID="ID_475540545" CREATED="1569947415903" MODIFIED="1569959464715">
+<icon BUILTIN="button_ok"/>
+<edge COLOR="#ff00ff"/>
+<node TEXT="add lapps node" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_740638920" CREATED="1569959079856" MODIFIED="1569959351450"/>
+<node TEXT="find example lapps" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1896016159" CREATED="1569959086332" MODIFIED="1569959359726"/>
+<node TEXT="find example directories" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1252759733" CREATED="1569959110839" MODIFIED="1569959364620"/>
+<node TEXT="save mm" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_246387930" CREATED="1569959119599" MODIFIED="1569959370132"/>
+<node TEXT="deploy" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_25227509" CREATED="1569959634893" MODIFIED="1569959638401">
+<node TEXT="generate html" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_351609812" CREATED="1569959126236" MODIFIED="1569959392835"/>
+<node TEXT="process html" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_757639651" CREATED="1569959134275" MODIFIED="1569959419979"/>
+<node TEXT="check in" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1047160617" CREATED="1569959138957" MODIFIED="1569959468520"/>
+<node TEXT="test" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_117495943" CREATED="1569959142446" MODIFIED="1569959585836"/>
+</node>
+<node TEXT="close issue" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_329771947" CREATED="1569959181527" MODIFIED="1569959593818"/>
+</node>
+<node TEXT="links" POSITION="left" ID="ID_836891301" CREATED="1569947416090" MODIFIED="1569947416167">
+<icon BUILTIN="attach"/>
+<edge COLOR="#00007c"/>
+<node TEXT="lightning money" ID="ID_1706699553" CREATED="1569947416169" MODIFIED="1569958999384" LINK="https://lightning.money/"/>
+</node>
+</node>
+</map>
