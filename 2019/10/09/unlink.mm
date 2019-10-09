f13ba753c5f0661a8c5679f087094497cde6fc64
Melvin Carvalho <melvincarvalho@gmail.com>
2019-10-09 14:34:14 +0200

    DEFAULT_MESSAGE

---
+++ 2019/10/09/unlink.mm
@@ -0,0 +1,132 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="unlink a fork" FOLDED="false" ID="ID_847715801" CREATED="1570624182012" MODIFIED="1570624197287" LINK="index.mm" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="14" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="summary" POSITION="right" ID="ID_209763013" CREATED="1570624186557" MODIFIED="1570624223063">
+<icon BUILTIN="idea"/>
+<edge COLOR="#007c00"/>
+<font BOLD="true"/>
+<node TEXT="find out how to unlink a fork" ID="ID_213722189" CREATED="1570624214188" MODIFIED="1570624219514"/>
+</node>
+<node TEXT="next" POSITION="right" ID="ID_599398509" CREATED="1570624186579" MODIFIED="1570624186594">
+<icon BUILTIN="go"/>
+<edge COLOR="#7c007c"/>
+<node TEXT="research" ID="ID_566406110" CREATED="1570624235141" MODIFIED="1570624236953">
+<node ID="ID_1089676526" CREATED="1570624321981" MODIFIED="1570624321981" LINK="https://github.community/t5/How-to-use-Git-and-GitHub/De-forking-a-Github-repo/td-p/2785"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://github.community/t5/How-to-use-Git-and-GitHub/De-forking-a-Github-repo/td-p/2785">https://github.community/t5/How-to-use-Git-and-GitHub/De-forking-a-Github-repo/td-p/2785</a> 
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="result" POSITION="right" ID="ID_454401641" CREATED="1570624186595" MODIFIED="1570624329249">
+<icon BUILTIN="bookmark"/>
+<edge COLOR="#007c7c"/>
+<node TEXT="There isn&apos;t a simple way to click a button and detach your forked repository from the parent. You can either create a new repository that is not a fork or you can delete the repository and recreate it as an empty one and add whatever you like to it." ID="ID_103005169" CREATED="1570624343578" MODIFIED="1570624351554">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node TEXT="I would probably recommend:" ID="ID_1458605643" CREATED="1570624343578" MODIFIED="1570624343578"/>
+<node TEXT="Create a new repository with a new name" ID="ID_1992299763" CREATED="1570624343602" MODIFIED="1570624359685">
+<icon BUILTIN="full-1"/>
+</node>
+<node TEXT="Set it up the way you want it including with your new scripts and the submodule configuration" ID="ID_455770591" CREATED="1570624343606" MODIFIED="1570624362943">
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="Make sure it is all working the way you like" ID="ID_982792653" CREATED="1570624343607" MODIFIED="1570624371194">
+<icon BUILTIN="full-3"/>
+</node>
+<node TEXT="(Optional) Delete the forked repo" ID="ID_689161567" CREATED="1570624343615" MODIFIED="1570624374564">
+<icon BUILTIN="full-4"/>
+</node>
+<node TEXT="(Optional) Rename the new repository to the same name as the forked repo" ID="ID_343510552" CREATED="1570624343617" MODIFIED="1570624378896">
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+<node TEXT="done" POSITION="left" ID="ID_233990147" CREATED="1570624186610" MODIFIED="1570624186625">
+<icon BUILTIN="button_ok"/>
+<edge COLOR="#7c7c00"/>
+</node>
+<node TEXT="later" POSITION="left" ID="ID_1567516123" CREATED="1570624186627" MODIFIED="1570624186644">
+<icon BUILTIN="prepare"/>
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="waiting" POSITION="left" ID="ID_1961117467" CREATED="1570624186645" MODIFIED="1570624186669">
+<icon BUILTIN="hourglass"/>
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="links" POSITION="left" ID="ID_740788425" CREATED="1570624186670" MODIFIED="1570624186712">
+<icon BUILTIN="attach"/>
+<edge COLOR="#00ff00"/>
+</node>
+</node>
+</map>
