45fe3d7927b478d62f46afab7b7f1a56ce3e564a
Melvin Carvalho <melvincarvalho@gmail.com>
2019-10-09 13:00:19 +0200

    DEFAULT_MESSAGE

--- bin/processhtml.sh
+++ bin/processhtml.sh
@@ -3,7 +3,9 @@
 #export DEFAULT_FILE="./index.html"
 #export FILE=${1:-$DEFAULT_FILE}
 
-for FILE in $(find . -name '*.html') 
+shopt -s globstar
+
+for FILE in **/*.html 
 do
 
     sed 's/\.mm"/.html"/g' "$FILE"  | sed 's/\.png" a/.svg" a/g' | sed 's/\ilink.svg/ilink.png/g' > /tmp/$$
